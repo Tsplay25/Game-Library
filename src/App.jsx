3f6e4f29e23e5cc0874f55c82437b8e79965619a
Talles <talves2503@gmail.com>
2023-11-18 21:04:48 -0300

    dumb component implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
-import { useState } from 'react';
 import Game from './components/Game';
 import NewGameForm from './components/NewGameForm';
+import useGameCollection from './hooks/useGameCollection';
 
 function App() {
-    const [games, setGames] = useState(() => {
-        const storedGames = localStorage.getItem('game-lib');
-        if (!storedGames) return [];
-        return JSON.parse(storedGames);
-    });
-
-    const addGame = ({ title, cover }) => {
-        const id = Math.floor(Math.random() * 100000);
-        const game = { id, title, cover };
-        setGames((state) => {
-            const newState = [...state, game];
-            localStorage.setItem('game-lib', JSON.stringify(newState));
-            return newState;
-        });
-    };
-
-    const removeGame = (id) => {
-        setGames((state) => {
-            const newState = state.filter((game) => game.id !== id);
-            localStorage.removeItem('game-lib', JSON.stringify(newState));
-            return newState;
-        });
-    };
+    const { games, addGame, removeGame } = useGameCollection();
 
     return (
         <div id="app">
             <h1>Bibiloteca de jogos</h1>
-            <NewGameForm addGame={addGame}/>
+            <NewGameForm addGame={addGame} />
             <div className="games">
                 {games.map((game) => (
                     <Game
